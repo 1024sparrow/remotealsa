b918be4e86a4d01d12001614518501e30d573c21
Gladish <jgladi200@cable.comcast.com>
2018-05-17 18:44:08 -0400

    More updates to ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 
-#include <QAudioDeviceInfo>
 #include <QAudioInput>
+#include <QHostAddress>
+#include <QSet>
 
 MainWindow::MainWindow()
   : m_socket(NULL)
+  , m_audioInMute(true)
+  , m_audioOutMute(true)
+  , m_audioInDeviceInfo(QAudioDeviceInfo::defaultInputDevice())
+  , m_audioReadBuffer(32768, 0)
 {
   createServerGroupBox();
   createAudioInGroupBox();
@@ -21,6 +26,8 @@ MainWindow::MainWindow()
   mainLayout->addWidget(m_feedbackText);
   mainLayout->addWidget(m_dialogButtonBox);
 
+  //m_audioOutput.reset(new QAudioOutput()
+
   setLayout(mainLayout);
   setWindowTitle("Sound Test");
 }
@@ -29,6 +36,21 @@ MainWindow::~MainWindow()
 {
 }
 
+void
+MainWindow::initializeAudio(QAudioDeviceInfo const& deviceInfo)
+{
+  QAudioFormat format;
+  format.setSampleRate(44100);
+  format.setChannelCount(2);
+  format.setSampleSize(16);
+  format.setCodec("audio/pcm");
+  format.setByteOrder(QAudioFormat::LittleEndian);
+  format.setSampleType(QAudioFormat::SignedInt);
+
+  m_audioOutput.reset(new QAudioOutput(deviceInfo, format));
+  m_audioOutDevice = m_audioOutput->start();
+  qDebug() << "output device started";
+}
 
 void
 MainWindow::createServerGroupBox()
@@ -37,12 +59,14 @@ MainWindow::createServerGroupBox()
 
   m_serverGroupBox = new QGroupBox("Server");
   m_connectButton = new QPushButton("Connect");
-  m_serverLineEdit = new QLineEdit("192.168.10.10");
+  m_serverAddressLineEdit = new QLineEdit("10.0.0.245");
+  m_serverPortLineEdit = new QLineEdit("10001");
 
   connect(m_connectButton, SIGNAL(released()), this, SLOT(connectButtonReleased()));
 
   layout->addWidget(m_connectButton);
-  layout->addWidget(m_serverLineEdit);
+  layout->addWidget(m_serverAddressLineEdit);
+  layout->addWidget(m_serverPortLineEdit);
   m_serverGroupBox->setLayout(layout);
 }
 
@@ -57,51 +81,147 @@ MainWindow::createAudioInGroupBox()
   m_audioInProgressBar->setRange(0, 10);
   m_audioInSelector = new QComboBox();
 
+  QSet<QString> set;
   QAudioDeviceInfo const& defaultDeviceInfo = QAudioDeviceInfo::defaultInputDevice();
   m_audioInSelector->addItem(defaultDeviceInfo.deviceName(), qVariantFromValue(defaultDeviceInfo));
   for (auto& deviceInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioInput))
   {
-    if (deviceInfo != defaultDeviceInfo)
+    qDebug() << "input audioDeviceInfo:" << deviceInfo.deviceName();
+    if ((deviceInfo != defaultDeviceInfo) && !set.contains(deviceInfo.deviceName()))
+    {
       m_audioInSelector->addItem(deviceInfo.deviceName(), qVariantFromValue(deviceInfo));
+      set.insert(deviceInfo.deviceName());
+    }
   }
 
   connect(m_audioInMuteButton, SIGNAL(released()), this, SLOT(audioInMuteButtonReleased()));
+  connect(m_audioInSelector, SIGNAL(activated(int)), this, SLOT(audioInDeviceChanged(int)));
 
   hlayout->addWidget(m_audioInMuteButton);
   hlayout->addWidget(m_audioInProgressBar);
   vlayout->addLayout(hlayout);
   vlayout->addWidget(m_audioInSelector);
   m_audioInGroupBox->setLayout(vlayout);
-
 }
 
 void
 MainWindow::createAudioOutGroupBox()
 {
-  QHBoxLayout* layout = new QHBoxLayout();
+  QVBoxLayout* vlayout = new QVBoxLayout();
+  QHBoxLayout* hlayout = new QHBoxLayout();
   m_audioOutGroupBox = new QGroupBox("Audio Out (From remote end)");
   m_audioOutMuteButton = new QPushButton("un-mute");
   m_audioOutProgressBar = new QProgressBar();
   m_audioOutProgressBar->setRange(0, 10);
+  m_audioOutSelector = new QComboBox();
+
+  QSet<QString> set;
+  QAudioDeviceInfo const& defaultDeviceInfo = QAudioDeviceInfo::defaultOutputDevice();
+  m_audioOutSelector->addItem(defaultDeviceInfo.deviceName(), qVariantFromValue(defaultDeviceInfo));
+  for (auto& deviceInfo : QAudioDeviceInfo::availableDevices(QAudio::AudioOutput))
+  {
+    qDebug() << "output audioDeviceInfo:" << deviceInfo.deviceName();
+    if ((deviceInfo != defaultDeviceInfo) && !set.contains(deviceInfo.deviceName()))
+    {
+      m_audioOutSelector->addItem(deviceInfo.deviceName(), qVariantFromValue(deviceInfo));
+      set.insert(deviceInfo.deviceName());
+    }
+  }
+
+  initializeAudio(defaultDeviceInfo);
 
   connect(m_audioOutMuteButton, SIGNAL(released()), this, SLOT(audioOutMuteButtonReleased()));
+  connect(m_audioOutSelector, SIGNAL(activated(int)), this, SLOT(audioOutDeviceChanged(int)));
 
-  layout->addWidget(m_audioOutMuteButton);
-  layout->addWidget(m_audioOutProgressBar);
-  m_audioOutGroupBox->setLayout(layout);
+  hlayout->addWidget(m_audioOutMuteButton);
+  hlayout->addWidget(m_audioOutProgressBar);
+  vlayout->addLayout(hlayout);
+  vlayout->addWidget(m_audioOutSelector);
+  m_audioOutGroupBox->setLayout(vlayout);
 }
 
 void
 MainWindow::connectButtonReleased()
 {
+  if (m_socket != nullptr)
+  {
+    m_socket->close();
+    delete m_socket;
+    m_connectButton->setText("Connect");
+  }
+  else
+  {
+    m_socket = new QTcpSocket();
+    connect(m_socket, SIGNAL(connected()), this, SLOT(onSocketConnected()));
+    connect(m_socket, SIGNAL(readyRead()), this, SLOT(onSocketReadyRead()));
+    m_socket->connectToHost(m_serverAddressLineEdit->text(), m_serverPortLineEdit->text().toUInt());
+    m_connectButton->setText("Disconnect");
+  }
 }
 
 void
 MainWindow::audioInMuteButtonReleased()
 {
+  if (m_audioInMute)
+  {
+    m_audioInMuteButton->setText("mute");
+    m_audioInMute = false;
+  }
+  else
+  {
+    m_audioInMuteButton->setText("un-mute");
+    m_audioInMute = true;
+  }
 }
 
 void
 MainWindow::audioOutMuteButtonReleased()
 {
+  if (m_audioOutMute)
+  {
+    m_audioOutMuteButton->setText("mute");
+    m_audioOutMute = false;
+  }
+  else
+  {
+    m_audioOutMuteButton->setText("un-mute");
+    m_audioOutMute = true;
+  }
+}
+
+void
+MainWindow::onSocketConnected()
+{
+  qDebug() << "connected to:" << m_socket->peerAddress()
+           << ":" << m_socket->peerPort();
+}
+
+void
+MainWindow::onSocketReadyRead()
+{
+  //qDebug() << "write:" << buff.size();
+  int chunks = m_audioOutput->bytesFree() / m_audioOutput->periodSize();
+  while (chunks) {
+    qint64 const len = m_socket->read(m_audioReadBuffer.data(), m_audioOutput->periodSize());
+    if (len && !m_audioOutMute)
+      m_audioOutDevice->write(m_audioReadBuffer.data(), len);
+    if (len != m_audioOutput->periodSize())
+        break;
+    -- chunks;
+  }
+}
+
+void
+MainWindow::audioInDeviceChanged(int index)
+{
+  qDebug() << "audioInDeviceChanged:" << index;
+}
+
+void
+MainWindow::audioOutDeviceChanged(int index)
+{
+  qDebug() << "audioOutDeviceChanged:" << index;
+
+  m_audioOutput->stop();
+  initializeAudio(m_audioOutSelector->itemData(index).value<QAudioDeviceInfo>());
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QtWidgets>
 #include <QTcpSocket>
+#include <QAudioDeviceInfo>
+#include <QScopedPointer>
+#include <QAudioOutput>
+#include <QAudioFormat>
 
 class MainWindow : public QDialog
 {
@@ -17,30 +21,47 @@ private:
   void createAudioInGroupBox();
   void createAudioOutGroupBox();
 
+  void playAudioData();
+  void initializeAudio(QAudioDeviceInfo const& deviceInfo);
+
 private slots:
   void connectButtonReleased();
   void audioInMuteButtonReleased();
   void audioOutMuteButtonReleased();
+  void audioInDeviceChanged(int index);
+  void audioOutDeviceChanged(int index);
 
+  // socket states
+  void onSocketConnected();
+  void onSocketReadyRead();
 
 private:
-  QGroupBox*   m_serverGroupBox;
-  QPushButton* m_connectButton;
-  QLineEdit*   m_serverLineEdit;
+  QGroupBox*                    m_serverGroupBox;
+  QPushButton*                  m_connectButton;
+  QLineEdit*                    m_serverAddressLineEdit;
+  QLineEdit*                    m_serverPortLineEdit;
 
-  QGroupBox*   m_audioInGroupBox;
-  QPushButton* m_audioInMuteButton;
-  QProgressBar*  m_audioInProgressBar;
-  QComboBox*   m_audioInSelector;
+  QGroupBox*                    m_audioInGroupBox;
+  QPushButton*                  m_audioInMuteButton;
+  QProgressBar*                 m_audioInProgressBar;
+  QComboBox*                    m_audioInSelector;
+  bool                          m_audioInMute;
+  QAudioDeviceInfo              m_audioInDeviceInfo;
 
-  QGroupBox*   m_audioOutGroupBox;
-  QPushButton* m_audioOutMuteButton;
-  QProgressBar* m_audioOutProgressBar;
+  QGroupBox*                    m_audioOutGroupBox;
+  QPushButton*                  m_audioOutMuteButton;
+  QProgressBar*                 m_audioOutProgressBar;
+  QComboBox*                    m_audioOutSelector;
+  bool                          m_audioOutMute;
+  QScopedPointer<QAudioOutput>  m_audioOutput;
+  QAudioFormat                  m_audioOutputFormat;
+  QIODevice*                    m_audioOutDevice;
+  QByteArray                    m_audioReadBuffer;
 
-  QTextEdit* m_feedbackText;
-  QDialogButtonBox* m_dialogButtonBox;
+  QTextEdit*                    m_feedbackText;
+  QDialogButtonBox*             m_dialogButtonBox;
 
-  QTcpSocket* m_socket;
+  QTcpSocket*                   m_socket;
 };
 
 #endif // MAINWINDOW_H
